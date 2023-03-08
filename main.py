c107ee00c843c5006a85c43078b43aaee6a0e2b5
jasonpopejoy <mail@jasonpopejoy.com>
2023-03-07 22:12:51 -0700

    pushin on up

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 name = input('What is your name? ')
 print('Hello ' + name)
+
+#Fundamental Data Types
+#int - whole numbers
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+print(2 ** 3)
+print(5 // 4) # gets rounded to the nearest int
+print(5 % 4) # divides and displays remainder 
+print(round(3.1)) #rounds the number
+print(abs(-21)) #returns absolute value of number (no negative)
+#float - floating point number (ie. has a decimal point)
+
+
+#bool
+#str
+#list
+#tuple
+#set
+#dict
+
+#Classes -> custom types
+
+#Specialized Data Types - modules
+
+#None 
+
