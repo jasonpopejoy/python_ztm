268091dfa304138e114a3a1c2f4c3faa621dd7ad
jasonpopejoy <mail@jasonpopejoy.com>
2023-03-08 17:46:47 -0700

    pushin on up

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print(round(3.1)) #rounds the number
 print(abs(-21)) #returns absolute value of number (no negative)
 #float - floating point number (ie. has a decimal point)
 #bool
-#str
+#str - string, ie a piece of text
 #list
 #tuple
 #set
@@ -73,3 +73,57 @@ print(c)
 #constants 
 PI = 3.14 # all caps to indicate it's a constant, good practice not to change the varialbe
 
+iq = 100 #this is also a statement
+user_age = iq / 5 #expression is 'iq / 5'
+#statement is 'user_age = iq / 5' the whole line of code
+
+#augmented assignment operator
+some_value = 5
+some_value += 2 #-= *= etc., operator goes to the left of the = sign
+print(some_value)
+
+counter = 0
+
+counter += 1
+counter += 1
+counter += 1
+counter += 1
+counter -= 1
+counter *=2
+
+#Before you click RUN, guess what the counter variable holds in memory!
+6
+print(counter)
+
+#strings
+print(type('Hi, hello there'))
+username = 'supercoder'
+password = 'supersecret'
+long_string = '''
+WOW
+O.O
+---
+'''
+print(long_string)
+#''' for multiline strings
+first_name = 'Derp'
+last_name = 'McGurp'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+#string concatination - only works with strings, not ints
+print('hello' + ' derp')
+
+#type conversion
+print(type(str(100))) #converted 100 into a string
+# ^ same as:
+a = str(100)
+b = type(a)
+print(b)
+
+#escape sequence
+weather = '\t It\'s "kind of" sunny \n hope you have a good day'
+print(weather)
+# \t tabs, \n newline
+
+#formatted strings
